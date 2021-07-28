d14fb7fc0d4f38b1a508d47dc62848254c1ac58d
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-07-28 12:56:03 +0100

    Add Locations handler

--- ArtistMapServer.go
+++ ArtistMapServer.go
@@ -9,9 +9,14 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to my web server!</h1>"))
 }
 
+func locationsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Locations!</h1>"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/locations", locationsHandler )
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ArtistMapServer
 Periodically checks wikidata and prepares map data.
+
+# Http Server
+
+Provides the following routes:
+
+- locations
+  - Responds with csv holding:
+    - Location Q-ID
+	 - Lat 
+	 - Lon
+	 - Artist Count
